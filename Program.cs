dc9710ec84ac14772f98f0546b3a392b7c51cd94
Vinícius Sarmento Costa Siqueira <vinicius.eng.comp92@gmail.com>
2023-05-07 23:07:39 -0300

    Adicionando calculo de raiz e potencia e alterando o nome de operador para tipo de cáalculo

--- Program.cs
+++ Program.cs
@@ -10,15 +10,17 @@ namespace calculadoraSimples
             Console.WriteLine("Calculadora C#\r");
             Console.WriteLine("------------------------\n");
 
-            Console.WriteLine("Operadores:");
+            Console.WriteLine("Tipo de Calculo:");
             Console.WriteLine("\t1 - Adição");
             Console.WriteLine("\t2 - Subtração");
             Console.WriteLine("\t3 - Multiplicação");
             Console.WriteLine("\t4 - Divisão");
             Console.WriteLine("\t5 - Porcentagem");
-            Console.Write("Escolha o operador? ");
+            Console.WriteLine("\t6 - Potência");
+            Console.WriteLine("\t7 - Raiz");
+            Console.Write("Escolha o tipo de Calculo? ");
 
-            int operador = int.Parse(Console.ReadLine()!);
+            int tipoCalculo = int.Parse(Console.ReadLine()!);
 
             Console.Write("Digite o primeiro número: ");
             double num1 = double.Parse(Console.ReadLine()!, CultureInfo.InvariantCulture);
@@ -32,7 +34,7 @@ namespace calculadoraSimples
                 Console.WriteLine("Não é possível dividir por zero.");
             else
             {
-                Calcular(num1, num2, operador, calc);
+                Calcular(num1, num2, tipoCalculo, calc);
 
                 Console.WriteLine($"{calc}");
             }
@@ -41,12 +43,12 @@ namespace calculadoraSimples
         static double Calcular(
             double num1,
             double num2,
-            int operador,
+            int tipoCalculo,
             Calculadora calc
         )
         {
             double resultado = 0;
-            switch (operador)
+            switch (tipoCalculo)
             {
                 case 1:
                     resultado = calc.Adicao(num1, num2);
@@ -62,6 +64,12 @@ namespace calculadoraSimples
                     break;
                 case 5:
                     resultado = calc.Porcentagem(num1, num2);
+                    break;
+                case 6:
+                    resultado = calc.Potencia(num1, num2);
+                    break;
+                case 7:
+                    resultado = calc.Raiz(num1, num2);
                     break;
                 default:
                     Console.WriteLine($"Número inválido digite outro número");
